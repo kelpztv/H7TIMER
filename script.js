d7ca4b70cd479beef9e0bee5608b70ed8f4aa8ed
kelpztv <kelpztv@gmail.com>
2026-01-19 02:21:50 +1000

    Add images and fixed schedules for bosses

--- script.js
+++ script.js
@@ -36,26 +36,52 @@ window.loginAdmin = () => {
 
 // ---------- Boss Data ----------
 const bosses = [
-  { name: "Venatus", interval: 10 },
-  { name: "Viorent", interval: 10 },
-  { name: "Ego", interval: 21 },
-  { name: "Livera", interval: 24 },
-  { name: "Araneo", interval: 24 },
-  { name: "Undomiel", interval: 24 },
-  { name: "Lady Dalia", interval: 18 },
-  { name: "General Aquleus", interval: 29 },
-  { name: "Amentis", interval: 29 },
-  { name: "Baron Braudmore", interval: 32 },
-  { name: "Shuliar", interval: 35 },
-  { name: "Larba", interval: 35 },
-  { name: "Catena", interval: 35 },
-  { name: "Gareth", interval: 32 },
-  { name: "Titore", interval: 37 },
-  { name: "Secreta", interval: 62 },
-  { name: "Ordo", interval: 62 },
-  { name: "Asta", interval: 62 },
-  { name: "Supore", interval: 62 },
+  { name: "Venatus", interval: 10, img: "venatus.jpg" },
+  { name: "Viorent", interval: 10, img: "viorent.jpg" },
+  { name: "Ego", interval: 21, img: "ego.jpg" },
+
+  { name: "Clemantis", fixed: ["Mon 13:30", "Thu 21:00"], img: "clemantis.jpg" },
+  { name: "Livera", interval: 24, img: "livera.jpg" },
+  { name: "Araneo", interval: 24, img: "araneo.jpg" },
+  { name: "Undomiel", interval: 24, img: "undomiel.jpg" },
+
+  { name: "Saphirus", fixed: ["Sun 19:00", "Tue 13:30"], img: "saphirus.jpg" },
+  { name: "Lady Dalia", interval: 18, img: "ladydalia.jpg" },
+
+  { name: "General Aquleus", interval: 29, img: "generalaquleus.jpg" },
+  { name: "Thymele", fixed: ["Mon 21:00", "Wed 13:30"], img: "thymele.jpg" },
+
+  { name: "Amentis", interval: 29, img: "amentis.jpg" },
+  { name: "Baron Braudmore", interval: 32, img: "baronbraudmore.jpg" },
+  { name: "Milavy", fixed: ["Sat 17:00"], img: "milavy.jpg" },
+
+  { name: "Metus", interval: 48, img: "metus.jpg" },
+  { name: "Duplican", interval: 48, img: "duplican.jpg" },
+
+  { name: "Shuliar", interval: 35, img: "shuliar.jpg" },
+  { name: "Ringor", fixed: ["Sat 19:00"], img: "ringor.jpg" },
+  { name: "Roderick", fixed: ["Fri 21:00"], img: "roderick.jpg" },
+
+  { name: "Gareth", interval: 32, img: "gareth.jpg" },
+  { name: "Titore", interval: 37, img: "titore.jpg" },
+  { name: "Larba", interval: 35, img: "larba.jpg" },
+
+  { name: "Catena", interval: 35, img: "catena.jpg" },
+  { name: "Auraq", fixed: ["Fri 00:00", "Wed 23:00"], img: "auraq.jpg" },
+
+  { name: "Secreta", interval: 62, img: "secreta.jpg" },
+  { name: "Ordo", interval: 62, img: "ordo.jpg" },
+  { name: "Asta", interval: 62, img: "asta.jpg" },
+  { name: "Supore", interval: 62, img: "supore.jpg" },
+
+  { name: "Chaiflock", fixed: ["Sat 00:00"], img: "chaiflock.jpg" },
+  { name: "Benji", fixed: ["Sun 23:00"], img: "benji.jpg" },
+
+  { name: "Icaruthia", fixed: ["Tue 23:00", "Fri 23:00"], img: "icaruthia.jpg" },
+  { name: "Motti", fixed: ["Wed 21:00", "Sat 21:00"], img: "motti.jpg" },
+  { name: "Nevaeh", fixed: ["Sun 00:00"], img: "nevaeh.jpg" }
 ];
+
 
 // ---------- Storage ----------
 const loadTime = (name) =>
